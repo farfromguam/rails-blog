05990dbdb0941f5b91f2b17dc8e8f7de5d006cc2
Christopher Fryman <farfromguam@gmail.com>
2013-06-04 10:44:34 -0500

    fixed test name

---
+++ test/integration/anonymous_browses_article_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class AnonymousBrowsesArticleTest < ActionDispatch::IntegrationTest
+  test "view a list of articles" do
+    Post.create!(title: "First Post!!", body: "This is awkward... I have nothing to say :(")
+    Post.create!(title: "Wow, it's been a while!", body: "And yet, I'm still not a writter :( :( lolol")
+
+    visit '/posts'
+    sleep 1
+    assert_include page.body, "First Post!!"
+    #=> assert page.content.include? "First Post!!"
+    assert_include page.body, "Wow, it's been a while!"
+  end
+end
--- test/integration/anonymous_post_article_test.rb
+++ test/integration/anonymous_post_article_test.rb
@@ -1,14 +1,7 @@
 require 'test_helper'
 
 class AnonymousPostArticleTest < ActionDispatch::IntegrationTest
-  test "view a list of articles" do
-    Post.create!(title: "First Post!!", body: "This is awkward... I have nothing to say :(")
-    Post.create!(title: "Wow, it's been a while!", body: "And yet, I'm still not a writter :( :( lolol")
+  test "posting a new article" do
 
-    visit '/posts'
-    sleep 1
-    assert_include page.body, "First Post!!"
-    #=> assert page.content.include? "First Post!!"
-    assert_include page.body, "Wow, it's been a while!"
   end
 end
