f743a97b5264f7878e14d6da4437df0dd69ecf80
Christopher Fryman <farfromguam@gmail.com>
2013-06-04 11:58:29 -0500

    basic posting works

--- test/integration/anonymous_browses_article_test.rb
+++ test/integration/anonymous_browses_article_test.rb
@@ -3,12 +3,12 @@ require 'test_helper'
 class AnonymousBrowsesArticleTest < ActionDispatch::IntegrationTest
   test "view a list of articles" do
     Post.create!(title: "First Post!!", body: "This is awkward... I have nothing to say :(")
-    Post.create!(title: "Wow, it's been a while!", body: "And yet, I'm still not a writter :( :( lolol")
+    Post.create!(title: "Wow, it has been a while!", body: "And yet, I'm still not a writter :( :( lolol")
 
     visit '/posts'
     sleep 1
     assert_include page.body, "First Post!!"
     #=> assert page.content.include? "First Post!!"
-    assert_include page.body, "Wow, it's been a while!"
+    assert_include page.body, "Wow, it has been a while!"
   end
 end
--- test/integration/anonymous_post_article_test.rb
+++ test/integration/anonymous_post_article_test.rb
@@ -2,6 +2,13 @@ require 'test_helper'
 
 class AnonymousPostArticleTest < ActionDispatch::IntegrationTest
   test "posting a new article" do
-
+    visit '/'
+    click_link 'Write Post'
+    fill_in 'Title', with: 'My First Post!!'
+    fill_in 'Body', with: 'I dont have anything to say'
+    click_button 'Submit Post'
+    assert_include page.body, 'Your post has now been published!'
+    visit '/posts'
+    assert_include page.body, 'My First Post!!'
   end
 end
